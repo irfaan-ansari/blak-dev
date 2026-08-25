6d05d377bd2fe1fe29dbc81e82f96eecde5461dd
Irfan Ansari <idevirfan@gmail.com>
2026-08-25 05:59:07 +0530

    refatcor(api): move health route to begining

--- services/api/src/app.ts
+++ services/api/src/app.ts
@@ -1,6 +1,6 @@
 import { Hono } from "hono"
 import { logger } from "hono/logger"
-import { corsMiddleware,  type AppContext } from "./middlewares"
+import { corsMiddleware, type AppContext } from "./middlewares"
 
 import auth from "./modules/auth/auth.routes"
 import v1 from "./routes/v1"
@@ -8,13 +8,12 @@ import v1 from "./routes/v1"
 export const createApp = () => {
   const app = new Hono<AppContext>()
 
+    .get("/health", (c) => c.json({ status: "ok" }))
     .use(logger())
 
     .use(corsMiddleware)
 
     .route("/auth", auth)
-
-    .get("/health", (c) => c.json({ status: "ok" }))
 
     .route("/v1", v1)
 
